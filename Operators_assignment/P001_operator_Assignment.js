ccb3a6b2bcac295581442163a2bc5eb54d4feae5
ranajoy545 <ranajoy545@gmail.com>
2026-09-03 22:02:17 +0530

    2nd commit

---
+++ Operators_assignment/P001_operator_Assignment.js
@@ -0,0 +1,67 @@
+//question 1
+
+console.log(1+"2"+3);//123
+//question 2
+console.log(1+2+"3");//33
+
+//question 3
+
+console.log(5*true);//5*1 =5
+
+//question 4
+
+console.log(10 > 5 > 1);//false
+
+console.log("------------------------------------")
+
+//question 5
+
+let a=2;
+
+let b = a++ + ++a;//2 + 4
+console.log(a);//4
+console.log(b);//6
+
+console.log("------------------------------------")
+//question 6 
+
+let c=3;
+
+let d = c++ + c++ + ++c;//3 + 4 + 6
+
+console.log(c);//6
+
+console.log(d);//13
+
+console.log("------------------------------------")
+
+//question 7
+
+let x=1;
+
+let y = ++x + x++ + x;//2 + 2 + 3
+
+console.log(x);//3
+
+console.log(y);//7
+
+
+console.log("------------------------------------")
+
+
+
+//question 8
+
+console.log(5*2 == "10");//true
+
+console.log("------------------------------------")
+//question 9
+
+console.log(10>5 && 20 <30);//true
+
+//question 10
+
+console.log("------------------------------------");
+console.log(null || undefined || "Playwright");//playwright
+
+
