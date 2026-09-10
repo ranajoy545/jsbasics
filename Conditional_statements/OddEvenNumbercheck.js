148af9c33ec6d195281def7f617f155a2acb2f9d
ranajoy545 <ranajoy545@gmail.com>
2026-09-10 16:49:00 +0530

    new commit

---
+++ Conditional_statements/OddEvenNumbercheck.js
@@ -0,0 +1,10 @@
+let num =34;
+
+if(num%2===0){
+
+    console.log("Number is even")
+}
+else{
+   console.log("number is odd");
+
+}
---
+++ Conditional_statements/Printdays.js
@@ -0,0 +1,31 @@
+let day = 5;
+
+switch (day) {
+    case 1:
+        console.log("It is monday");
+        break;
+
+    case 2:
+        console.log("It is Tuesday");
+        break;
+    case 3:
+        console.log("It is wednesday");
+        break;
+
+        case 4:
+             console.log("it is thursday");
+             break;
+           
+       case 5:
+           console.log("it is friday");
+           break;
+
+         case 6:
+             console.log("it is saturday");
+           break; 
+        case 7:
+             console.log("it is sunday ");
+             break;
+    default:
+        break;
+}
